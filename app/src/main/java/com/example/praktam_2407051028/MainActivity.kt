ffd6a49319ee215ee102403215894e2cefe69b28
Qorina Qisthi Kamal <qorinaqisthykamaltubaba@gmail.com>
2026-03-30 21:26:55 +0700

    Initial commit: Tugas LKP 4

--- app/src/main/java/com/example/praktam_2407051028/MainActivity.kt
+++ app/src/main/java/com/example/praktam_2407051028/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,12 +13,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.praktam_2407051028.ui.theme.PrakTAM_2407051028Theme
 import androidx.compose.foundation.Image
@@ -61,17 +73,32 @@ fun FashionList() {
 }
             @Composable
             fun DetailFashion(fashion: Fashion) {
-                Column(modifier = Modifier.fillMaxWidth()) {
+                var isFavorite by remember {mutableStateOf(false)}
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Box {
+                        Image(
+                            painter = painterResource(id = fashion.imageRes),
+                            contentDescription = fashion.nama,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(200.dp),
+                            contentScale = ContentScale.Crop
+                        )
 
-                    Image(
-                        painter = painterResource(id = fashion.imageRes),
-                        contentDescription = fashion.nama,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(200.dp),
-                        contentScale = ContentScale.Crop
-                    )
-
+                        IconButton(
+                            onClick = { isFavorite = !isFavorite },
+                            modifier = Modifier
+                                .align(Alignment.TopEnd)
+                                .padding(8.dp)
+                        )
+                        {
+                            Icon(
+                                imageVector = if (isFavorite) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
+                                contentDescription = "Favorite Icon",
+                                tint = if (isFavorite) Color.Red else Color.White
+                            )
+                        }
+                    }
                     Spacer(modifier = Modifier.height(16.dp))
 
                     Text(
